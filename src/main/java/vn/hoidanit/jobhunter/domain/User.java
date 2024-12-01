d41cb5e936cdcae62d80d461969506bef8813c8c
hhhuuunnnggg-design <nguyendinhhungtc2020@gmail.com>
2024-12-01 23:02:48 +0700

    .

--- src/main/java/vn/hoidanit/jobhunter/domain/User.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/User.java
@@ -2,13 +2,23 @@ package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import vn.hoidanit.jobhunter.domain.Enum.GenderEnum;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
-
 
 @Entity
 @Table(name = "users")
@@ -41,6 +51,10 @@ public class User {
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
 
     @PrePersist
     public void handleBeforeCreate() {
